d2784d2b1bef89e7441099560143b16f4e04a410
Evandro <evandrosanches62@gmail.com>
2022-05-29 12:33:42 -0300

    iniciando estrutura de banco de dados

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=30)
+    rg = models.CharField(max_length=9)
+    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+class Curso(models.model):
+    pass
